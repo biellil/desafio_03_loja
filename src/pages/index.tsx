3b48258e220878e69776b9b04ce7616d4184c362
biel.lil <78098857+biellil@users.noreply.github.com>
2023-05-16 18:00:56 -0300

    Estilização CSS com SASS
     Estilização CSS com SASS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
-  return (
-   <><h1>home</h1></>
+  return (<h1 className={styles.title}>Olá <span> Dev!</span> </h1>
   )
 }
