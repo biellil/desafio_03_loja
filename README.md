b14319429a770f4d7289072639ea77b07866c2b4
biel.lil <78098857+biellil@users.noreply.github.com>
2023-05-17 14:36:26 -0300

    18/05
    Nota de atualização: 18/05 Client Side Rendering E criando JSON Serve - API teste

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 First, run the development server:
 
 ```bash
+npx json-server api.json -p 3333 -w
 npm run dev
 # or
 yarn dev
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
 
+import { useEffect, useState } from 'react';
 import styles from '../styles/home.module.scss';
 
+interface Post{
+  id : string;
+  title: string;
+}
 export default function Home() {
-  return (<h1 className={styles.title}>Olá <span> Dev!</span> </h1>
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/posts').then(response => {
+      response.json().then(data => {
+        setPosts(data);
+      });
+    });
+  }, []);
+return(
+  <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
